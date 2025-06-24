f48135f491687bdcc358a0bb07b3529165949e1a
oscartflores <oflores1502@gmail.com>
2025-06-23 17:09:36 -0700

    Added character counter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from stats import word_count
-
+from stats import char_count
 def main():
     book = word_count("frankenstein.txt")
     print(f"{book} words found in the document")
--- stats.py
+++ stats.py
@@ -1,7 +1,29 @@
 def word_count(path):
-    files_contents=""
     with open(f"books/{path}") as f:
         files_contents = f.read()
     split_contents = files_contents.split()
     count = len(split_contents)
     return count
+
+def char_count(path):
+    character_dict = {}
+    with open(f"books/{path}") as f:
+        files_contents = f.read()
+        lower_case = files_contents.lower()
+        for letters in lower_case:
+            if letters not in character_dict:
+                character_dict[letters] = 1
+            else:
+                character_dict[letters] += 1
+    return character_dict
+
+    
+
+
+
+def main():
+    book = char_count("frankenstein.txt")
+    print(book)
+
+main()
+    
